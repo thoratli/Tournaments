71d7ddeee8d7bdeb8069e413405b01740b999923
AtliThorStor <atlits18@ru.is>
2019-09-07 13:52:12 +0000

    basic functions

--- Tournaments.py
+++ Tournaments.py
@@ -2,25 +2,30 @@
 
 class Tournament:
     def __init__(self):
-        self.players_list = []
+        self.players_list = {}
         self.rounds = 0
         self.players = 0
-        self.total_players = self.get_total_players()
-        self.total_rounds = self.get_rounds()
+        self.total_players = self.__get_total_players__()
+        self.total_rounds = self.__get_rounds__()
+        self.players_list = self.__get_players_name__()
+        print(self.__print_starter__())
 
 
 
-    def get_total_players(self):
+    def __get_total_players__(self):
 
         while True:
             try:
                 self.players = int(input("How many players will participate? "))
-                return
+                return self.players
             except:
                 print("Please enter a number! ")
 
 
-    def get_players_name(self):
+    def __get_players_name__(self):
+
+        for i in range(int(self.total_players)):
+            self.players_list[i+1] = input(f'Participant nr {i+1}: ')
 
 
     def __get_names__(self):
@@ -29,13 +34,22 @@ class Tournament:
             self.players_list.append(name)
 
 
-
-    def get_rounds(self):
+    def __get_rounds__(self):
         while True:
             try:
                 number = int(input("How many rounds you want to play? "))
                 if number > 0:
-                    self.players = number
-                    return
+                    return number
             except:
                 print("Please enter a number! ")
+
+
+    def __total_games__(self):
+        return f'{(round(self.total_players/2)*(self.total_players-1))}'
+
+
+    def __print_starter__(self):
+        retval = f'So {self.total_players} players are competing.\n'
+        retval += f'You wanted to play {self.total_rounds} rounds.\n'
+        retval += f'In total you will play {self.__total_games__()} games'
+        return retval
