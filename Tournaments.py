3b524580f1a16445ca1667a8af337a1f14496b7f
AtliThorStor <atlits18@ru.is>
2019-09-07 13:28:06 +0000

    Starting functions

--- Tournaments.py
+++ Tournaments.py
@@ -1,25 +1,41 @@
+
+
 class Tournament:
     def __init__(self):
+        self.players_list = []
         self.rounds = 0
         self.players = 0
+        self.total_players = self.get_total_players()
+        self.total_rounds = self.get_rounds()
 
 
 
-    def get_players(self):
+    def get_total_players(self):
+
         while True:
             try:
                 self.players = int(input("How many players will participate? "))
                 return
             except:
-                print("Please enter a number: ")
+                print("Please enter a number! ")
 
 
-        pass
+    def get_players_name(self):
 
-    def get_games(self):
-        # Todo: implement
-        pass
+
+    def __get_names__(self):
+        for name in range(self.players):
+
+            self.players_list.append(name)
+
+
 
     def get_rounds(self):
-        # Todo: implement
-        pass
+        while True:
+            try:
+                number = int(input("How many rounds you want to play? "))
+                if number > 0:
+                    self.players = number
+                    return
+            except:
+                print("Please enter a number! ")
