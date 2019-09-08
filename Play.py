5270607910976fb79a2742f5a8cb5f580cc85ab6
AtliThorStor <atlits18@ru.is>
2019-09-08 00:26:56 +0000

    coming closer to fixtures

--- Play.py
+++ Play.py
@@ -1,5 +1,9 @@
 from Tournaments import Tournament
 
+TEAMS = ['Real Madrid', 'Barcelona', 'Liverpool', 'PSG', 'Brazil', 'Iceland', 'Pick 2starred team',
+         'Italy', 'Belgium', 'Pick a 3starred team', 'Fifa Random Rule', 'Your competitor picks for you',
+         'Scandinavia', 'Spain', 'Netherland', 'Pick 2 star lower than your opponent']
+
 class Play:
 
     def __init__(self):
@@ -9,26 +13,47 @@ class Play:
         self.games = mot.__total_games__()
         self.players_dict = mot.players_list
         print(mot.__print_starting_info__())
-        self.game_on = 0
-        # self.tuples = mot.create_pair_of_tubles()
-        self.fixtures = mot.fixtures
+        self.fixtures = {}
+        self.fixtures = mot.get_fixtures()
         self.print_fixtures()
 
 
     def start_tournament(self):
-        while self.game_on < int(self.games):
-            print(f'Leikur {self.game_on + 1} í gangi')
-            self.game_on += 1
+        game_on = 0
+
+        while game_on < int(self.games):
+            game_on += 1
+
+            print(f'PLAYING GAME NR: {game_on}')
+            print(self.fixtures[game_on])
+            score = self.get_scores_from_input()
+            self.fixtures[game_on][1] = score
+
+        print(self.fixtures
+              )
+        #todo: implement how to keep score
 
 
     def print_fixtures(self):
-        for key,value in self.fixtures.items():
-            print(f'Game nr: {key}, Teams: {value}')
+        pass
+        # for key,value in self.fixtures.items():
+        #     print(f'Game nr:{key}, Teams: {value}')
+
             #implement the names in the value
+            #implement pretty print
+
 
     def get_scores_from_input(self):
         #Todo: get input from user, the score of a game and store it somehow
-        pass
+        while True:
+            try:
+                score = input("Enter the scores with space between, 2 2: ").split()
+                if len(score) == 2:
+                    return score
+                else:
+                    print("Please try again, bitch!")
+            except:
+                print("Please try again, and be careful.")
 
 
     def show_league_table(self):
