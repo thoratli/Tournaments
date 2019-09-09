55d3c31039e24c62d3b0363683c9f5bb38411bfc
AtliThorStor <atlits18@ru.is>
2019-09-09 21:40:43 +0000

    adding get_options func

--- options.py
+++ options.py
@@ -2,11 +2,16 @@
 from tournament import *
 
 class Options:
+
     def __init__(self):
-        self.show_options()
+        self.total_options = 3
 
     def show_options(self):
-        retval = "\n\n[1] Create Tournament\n"
-        retval += "[2] Print current League\n"
-        retval += "[3] Play next game in current Tournament"
+        retval = "\n[1] Print current League\n"
+        retval += "[2] Play next game in current Tournament\n"
+        retval += "[3] Print stats"
         return retval
+
+    def get_option(self):
+        option = input("Enter an option: ")
+        return option
