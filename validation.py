4eec63d8bcaad3a7bfa945331cd5a4781ba48367
AtliThorStor <atlits18@ru.is>
2019-09-09 00:22:20 +0000

    adding validation on input

--- validation.py
+++ validation.py
@@ -2,15 +2,27 @@
 
 class Validation():
 
-    def validate_integer(self):
+    def validate_integer(self, string: str):
+
+        try:
+            int(string)
+            return True
+        except:
+            print("Please enter an integer! ")
+            return False
+
+
+    def validate_score_input(self, string: str):
         pass
 
-    def validate_score_input(self):
-        pass
+    def validate_name_input(self, string: str):
+        namelist = string.split()
+        if len(namelist) == 1:
+            return True
+        else:
+            print("Please, just one name! ")
+            return False
 
-    def validate_name_input(self):
-        pass
-
-    def validate_rounds(self):
+    def validate_rounds(self, integer: int):
         pass
 
