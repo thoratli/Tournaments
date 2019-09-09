05349d33e655bb3fb677f690beeceb5dd0c4a8d4
AtliThorStor <atlits18@ru.is>
2019-09-09 00:21:39 +0000

    adding validation and print league

--- Tournaments.py
+++ Tournaments.py
@@ -1,29 +1,50 @@
+from validation import Validation
 
-
-class Tournament:
+class Tournament():
     def __init__(self):
+        self.name = self.__get_tournament_name()
+        self.validate = Validation()
         self.players_list = {}
         self.rounds = 0
-        self.players = 0
+        # self.players = 0
         self.total_players = self.__get_total_players__()
         self.total_rounds = self.__get_rounds__()
         self.__get_players_name__()
+
+    def __get_tournament_name(self):
+        name = input("What is the name of your League?\n ")
+        return name
+
+    def print_league(self):
+        #todo: implement values in the print
+
+        print(f'\n                  ---- {self.name} ----')
+        print("PLAYER       PLAYED     SCORED    CONCEDED    +/-    POINTS")
+
+        for key,value in self.players_list.items():
+            print(f'Player {key}        x          X         Y        X-Y      3 ')
+        print("\n\n")
 
 
     def __get_total_players__(self):
 
         while True:
-            try:
-                self.players = int(input("How many players will participate? "))
-                return self.players
-            except:
-                print("Please enter a number! ")
+            players = input("How many players: ")
+            if self.validate.validate_integer(players):
+                return int(players)
 
 
     def __get_players_name__(self):
 
+        players = 0
+        #todo implement id system for user
+
         for i in range(int(self.total_players)):
-            self.players_list[i+1] = (input(f'Participant nr {i+1}: '))
+            while players == i:
+                player = input(f'Participant nr {players +1}: ')
+                if self.validate.validate_name_input(player):
+                    self.players_list[i+1] = player
+                    players += 1
 
 
     def __get_rounds__(self):
@@ -55,7 +76,7 @@ class Tournament:
 
 
     def __print_starting_info__(self):
-        retval = f'So {self.total_players} players are competing.\n'
+        retval = f'\nSo {self.total_players} players are competing.\n'
         retval += f'You wanted to play {self.total_rounds} rounds.\n'
         retval += f'In total you will play {self.__total_games__()} games\n'
         return retval
