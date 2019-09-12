72983dbe3894176ef37a794c1a0a9acae230d440
AtliThorStor <atlits18@ru.is>
2019-09-12 00:06:41 +0000

    pretty print for options

--- options.py
+++ options.py
@@ -7,10 +7,10 @@ class Options:
         self.total_options = TOTAL_OPTIONS
 
     def show_options(self):
-        retval = "\n[1][ENTER] PLAY NEXT GAME\n"
-        retval += "[2] Show fixtures \n"
-        retval += '[3] Show table - missing values\n'
-        retval += "[4] Print stats - not implemented\n"
+        retval = "\n------------------------\n[ENTER] PLAY NEXT GAME\n------------------------\n"
+        retval += "[1] Show fixtures \n"
+        retval += '[2] Show table - missing values\n'
+        retval += "[3] Print stats - not implemented\n"
         return retval
 
     def get_option(self):
