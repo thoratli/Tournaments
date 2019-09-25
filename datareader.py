f82e2f4713c558e73409597c69aca0951840edae
AtliThorStor <atlits18@ru.is>
2019-09-25 00:46:25 +0000

    reader for csv file

--- datareader.py
+++ datareader.py
@@ -5,47 +5,60 @@ TOURNAMENT_NAME = 0
 ID = 1
 ROUNDS = 2
 TOTAL_PLAYERS = 3
-PLAYER_NAME = 4
-FORM = 5
+FORM = 4
+PLAYER_NAME = 5
 TEAM = 6
 PLAYED = 7
 SCORED = 8
 CONCEDED = 9
 DIFFERENCE = 10
 POINTS = 11
-HEADER_FOR_TOURNAMENT = 5
-HEADER_PLAYERS_LOWER = 6
+
+#print the first 4 cols from csv
+HEADER_FOR_TOURNAMENT_LOWER = 0
+HEADER_FOR_TOURNAMENT_HIGHER = 4
+
+#print the last rows from cols
+HEADER_PLAYERS_LOWER = 5
 HEADER_PLAYERS_HIGHER = 10
 
 
 class abstract_model():
-    def __init__(self):
+    def __init__(self, filename):
+        self.file_name = filename
         self.file_list = []
         self.tournaments = {}
         self.players_data = {}
 
+    def get_all_data(self, key_in):
 
-    def read_data(self,filename):
+        attribute_list = []
+        for key,value in self.tournaments.items():
+            if int(key) == int(key_in):
+                name = value[TOURNAMENT_NAME]
+                id = value[ID]
+                rounds = value[ROUNDS]
+                total_players = value[TOTAL_PLAYERS]
+                form = value[FORM]
+
+                attribute_list.append(name)
+                attribute_list.append(id)
+                attribute_list.append(rounds)
+                attribute_list.append(total_players)
+                attribute_list.append(form)
+
+                return attribute_list
+
+        # return self.tournaments[key], self.players_data[key]
+
+    def read_data(self):
         """Reads data from given csvfile"""
 
-        with open(filename, 'r', encoding='utf-8-sig', newline='') as src:
+        with open(self.file_name, 'r', encoding='utf-8-sig', newline='') as src:
             reader = csv.reader(src, dialect='excel')
 
             for row in reader:
                 self.file_list.append(row)
-
-    def _print_headers_for_data(self, minlimit: int, maxlimit=None):
-        """Prints out the headers of the csv file, minlimit 4 for tournament,
-        minlimit 5 for players and maxlimit 10"""
-
-        for index, value in enumerate(self.file_list[0]):
-            if maxlimit:
-                if minlimit<=index<=maxlimit:
-                    print(value, end=" ")
-            else:
-                if index <= minlimit:
-                    print(value, end=" ")
-        print("\n")
 
     def get_tournament_data(self):
 
@@ -58,6 +71,7 @@ class abstract_model():
             rounds = self.file_list[tournament_number][ROUNDS]
             total_players = self.file_list[tournament_number][TOTAL_PLAYERS]
             form = self.file_list[tournament_number][FORM]
+
             player_name = self.file_list[tournament_number][PLAYER_NAME]
             team = self.file_list[tournament_number][TEAM]
             played = self.file_list[tournament_number][PLAYED]
@@ -67,8 +81,9 @@ class abstract_model():
             points = self.file_list[tournament_number][POINTS]
 
             #entering the data into the dict
-            self.tournaments[id] = [id]
+            self.tournaments[id] = []
             self.tournaments[id].append(name)
+            self.tournaments[id].append(id)
             self.tournaments[id].append(rounds)
             self.tournaments[id].append(total_players)
             self.tournaments[id].append(form)
@@ -85,6 +100,16 @@ class abstract_model():
 
             tournament_number += 1
 
+    def _print_headers_for_data(self, minlimit: int, maxlimit: int):
+        """Prints out the headers of the csv file, minlimit 4 for tournament,
+        minlimit 5 for players and maxlimit 10"""
+
+        print("                        ", end="")
+        for index, value in enumerate(self.file_list[0]):
+            if minlimit <= index <= maxlimit:
+                print("", value, end=" ")
+        print("\n")
+
     def print_tournament_data(self):
         """Prints out tournament data from database"""
 
@@ -92,12 +117,10 @@ class abstract_model():
         print("These are the Tournaments in the database with all data:\n")
         print("---------------------------------------------------------\n")
 
-        self._print_headers_for_data(HEADER_FOR_TOURNAMENT)
-
+        self._print_headers_for_data(HEADER_FOR_TOURNAMENT_LOWER, HEADER_FOR_TOURNAMENT_HIGHER)
 
         for key,value in self.tournaments.items():
-            print("Tournament ID:", key, ",Tournament data:", value)
-        print("\n")
+            print("Tournament:",key, ", Data:", value)
 
     def print_player_data(self):
         """Prints out players data from database"""
@@ -113,8 +136,8 @@ class abstract_model():
             print("Player:", key, " Attributes:", value)
 
 
-model = abstract_model()
-read = model.read_data('data.csv')
-model.get_tournament_data()
-model.print_tournament_data()
-model.print_player_data()
+# model = abstract_model()
+# read = model.read_data('data.csv')
+# model.get_tournament_data()
+# model.print_tournament_data()
+# model.print_player_data()
