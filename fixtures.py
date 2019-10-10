801f7c447648562978e442290364c4c7f0e04cee
AtliThorStor <atlits18@ru.is>
2019-10-10 14:23:14 +0000

    fixtures seems to be okay

--- fixtures.py
+++ fixtures.py
@@ -5,7 +5,7 @@ class Fixtures():
         self.fixtures = {}
 
     def generate_fixture_list(self, teams: list):
-        if len(teams) % 2:
+        if len(teams) % 2 != 0:
             teams.append('Day off')
         n = len(teams)
         match = []
@@ -13,11 +13,10 @@ class Fixtures():
 
         return_match = []
         for fixture in range(1, n):
-            for i in range(round(n)):
+            for i in range(round(n/2)):
                 match.append((teams[i], teams[n - 1 - i]))
                 return_match.append((teams[n - 1 - i], teams[i]))
             teams.insert(1, teams.pop())
-            fixtures.insert(round(len(fixtures)), match)
             fixtures.append(return_match)
             match = []
             return_match = []
@@ -26,13 +25,13 @@ class Fixtures():
         return fixtures
 
     def _insert_fixture_into_dict(self, fixtures: list):
-        round = 1
         game = 1
 
         for fixture in fixtures:
             for i in fixture:
-                self.fixtures[game] = [i, []]
-                game += 1
+                if 'Day off' not in i:
+                    self.fixtures[game] = [i, []]
+                    game += 1
 
         for key,value in self.fixtures.items():
             print(key, value)
